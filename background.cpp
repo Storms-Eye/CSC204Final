c4b7179135700b8c74c4abec5e7fa0016e820e43
Bradimus22 <bradycolbert0@gmail.com>
2026-04-13 11:44:20 -0500

    Update background.cpp

--- background.cpp
+++ background.cpp
@@ -1,1 +1,19 @@
+#include "background.h"
+#include "draw_utils.h"
 
+extern GLuint backgroundID;
+
+void Tree::draw(float tree_x, float tree_y)
+{
+    float cx = tree_x;
+    float cy = tree_y;
+
+    glPushMatrix();
+    glTranslatef(cx, cy, 0.0f);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, treeID); // Assuming this is accessible
+    texturedQuad(0.0f, 0.0f, FROG_SIZE, FROG_SIZE);
+    glDisable(GL_TEXTURE_2D);
+    glPopMatrix();
+}
